757f68d34ab7324f473dc153aa746d609ed69249
vikalp <avikalp233@gmail.com>
2025-04-14 11:38:50 +0530

    Basic Layout

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return <div className="">
+    I will load videos in the future
+  </div>
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return <div className="font-bold text-rose-500">
-    <Button>Hello world</Button>
-  </div>
-}
